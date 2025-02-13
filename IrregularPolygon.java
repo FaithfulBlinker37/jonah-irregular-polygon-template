2293a96dd52b94a13eefcf3dc63f9a05feca5b93
FaithfulBlinker37 <jonahwang26@gmail.com>
2025-02-13 22:19:10 +0000

    commit 5

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -23,14 +23,14 @@ public class IrregularPolygon {
         if (myPolygon.size() < 2) {
             return 0.0;
         }
-
+    
         double perimeter = 0.0;
         for (int i = 0; i < myPolygon.size(); i++) {
             Point2D.Double current = myPolygon.get(i);
             Point2D.Double next = myPolygon.get((i + 1) % myPolygon.size());
             perimeter += current.distance(next);
         }
-        return 3.14;
+        return perimeter;
     }
 
     public double area() {
